d3df2feea93d5e43b9ff992d8ec8a7c816cbbde3
J <walter13@qq.com>
2019-07-03 17:56:12 +0800

    新增entity

--- src/main/java/com/ral/manages/config/WebSecurityConfig.java
+++ src/main/java/com/ral/manages/config/WebSecurityConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.ral.manages.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -14,3 +15,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         super.configure(web);
     }
 }
+*/
