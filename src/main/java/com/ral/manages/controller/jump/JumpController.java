33dc81fe00dd926e39b610a4e86b5a69650af84e
J <walter13@qq.com>
2019-08-21 11:17:07 +0800

    修改html

--- src/main/java/com/ral/manages/controller/jump/JumpController.java
+++ src/main/java/com/ral/manages/controller/jump/JumpController.java
@@ -3,6 +3,7 @@ package com.ral.manages.controller.jump;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *   <p>功能描述：页面跳转</p>
@@ -18,24 +19,24 @@ public class JumpController {
     public String user(@PathVariable String name){ return name; }
 
     @RequestMapping("/404")
-    public String jumpError(){ return "/error/404.html"; }
+    public String jumpError(){ return "error/404.html"; }
 
     @RequestMapping("/account")
-    public String jumpAccount(){ return "/app/account.html"; }
+    public String jumpAccount(){ return "app/account.html"; }
 
     @RequestMapping("/school")
-    public String jumpSchool(){ return "/app/school.html"; }
+    public String jumpSchool(){ return "app/school.html"; }
 
     @RequestMapping("/effect")
-    public String jumpEffect(){ return "/app/effect.html"; }
+    public String jumpEffect(){ return "app/effect.html"; }
 
     @RequestMapping("/move")
-    public String jumpZhaoShi(){ return "/app/move.html"; }
+    public String jumpZhaoShi(){ return "app/move.html"; }
 
     @RequestMapping("/kongFu")
-    public String jumpKongFu(){ return "/app/kongfu.html"; }
+    public String jumpKongFu(){ return "app/kongfu.html"; }
 
     @RequestMapping("/article")
-    public String jumpCharacter(){ return "/app/article.html"; }
+    public String jumpCharacter(){ return "app/article.html"; }
 
 }
---
+++ src/main/java/com/ral/manages/controller/jump/VerTokenController.java
@@ -0,0 +1,46 @@
+package com.ral.manages.controller.jump;
+
+import com.ral.manages.commom.exception.AesException;
+import com.ral.manages.util.SHA1Util;
+import com.ral.manages.util.SetUtil;
+import com.ral.manages.util.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class VerTokenController {
+    private static final Logger LOG = LoggerFactory.getLogger(VerTokenController.class);
+
+    @RequestMapping("/")
+    @ResponseBody
+    public Object verToken(HttpServletRequest request){
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        if(StringUtil.isNull(signature) || StringUtil.isNull(timestamp) || StringUtil.isNull(nonce) || StringUtil.isNull(echostr)){
+            return "start task.......";
+        }else {
+            System.out.println("signature：" + signature + "，echostr：" + echostr);
+            String token = "aiurorigin";
+            String result = "";
+            try {
+                result = SHA1Util.getSHA1(token, timestamp, nonce);
+            } catch (AesException e) {
+                LOG.info(e.getMessage(), e);
+            }
+            System.out.println("result:" + result);
+            if (signature.equals(result)) {
+                return echostr;
+            } else {
+                return false;
+            }
+        }
+    }
+}
